58a826b551dd30727394a96d1d378240dfe44c19
Maulik Mann <maulikmann@gmail.com>
2024-12-16 20:14:32 -0500

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import nightModeIcon from './pics/dark-mode.png';
 import sunIcon from './pics/sun.png';
 import sendIcon from './pics/send.png';
 import avatarpic from './pics/user.png'
-
+// test
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, orderBy, query, addDoc, serverTimestamp, limit } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
